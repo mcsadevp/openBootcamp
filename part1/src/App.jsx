c76a0d7692212a14cebd97ab70977dc2dd237c07
Mc. Saavedra <Mcsaavedra23@gmail.com>
2024-09-12 21:42:18 -0300

    ejercicio 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,15 +4,17 @@ import TotalComponent from "./component/TotalComponent"
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = { name: 'Fundamentals of React', exercises: 10 };
-  const part2 = { name: 'Using props to pass data', exercises: 7 };
-  const part3 = { name: 'State of a component', exercises: 14 };
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
 
   return (
     <>
-      <HeadComponent course={course} />
-      <ContentComponent part1={part1} part2={part2} part3={part3} />
-      <TotalComponent part1={part1} part2={part2} part3={part3} />
+     <HeadComponent course={course} />
+      <ContentComponent parts={parts} />
+      <TotalComponent parts={parts} />
 
     </>
   )
