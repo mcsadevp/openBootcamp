ab69a9ee0f6f2199f0c682269fd2c06a5ba4d6ec
Mc. Saavedra <Mcsaavedra23@gmail.com>
2024-09-12 21:40:01 -0300

    ejercicio 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,26 +1,19 @@
 import ContentComponent from "./component/ContentComponent"
-import HeadCopmponent from "./component/HeadCopmponent"
-import PartComponent from "./component/PartComponent"
+import HeadComponent from "./component/HeadComponent"
 import TotalComponent from "./component/TotalComponent"
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+  const part1 = { name: 'Fundamentals of React', exercises: 10 };
+  const part2 = { name: 'Using props to pass data', exercises: 7 };
+  const part3 = { name: 'State of a component', exercises: 14 };
 
   return (
     <>
-       <HeadCopmponent course={course} />  
-        <ContentComponent
-          part1={part1} exercises1={exercises1}
-          part2={part2} exercises2={exercises2}
-          part3={part3} exercises3={exercises3} />
-      <TotalComponent exercises1 = {exercises1}  exercises2={exercises2} exercises3={exercises3} />
-   
+      <HeadComponent course={course} />
+      <ContentComponent part1={part1} part2={part2} part3={part3} />
+      <TotalComponent part1={part1} part2={part2} part3={part3} />
+
     </>
   )
 }
--- part1/src/component/TotalComponent.jsx
+++ part1/src/component/TotalComponent.jsx
@@ -1,15 +1,12 @@
 
 function TotalComponent(props) {
     return (
-        <>
             <p>Number of exercises 
             {props.exercises1 +
             props.exercises2 +
             props.exercises3}
             </p>
-        </>
     )
 }
-
 
 export default TotalComponent
